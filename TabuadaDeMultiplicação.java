5fd5b3cea3f24de3616f7df24990ceecf4d7af5b
nick-bastos <nick.l.bastos11@gmail.com>
2022-09-14 16:53:31 -0300

    usando laço for

--- TabuadaDeMultiplicação.java
+++
@@ -1,5 +0,0 @@
-package Praticando;
-
-public class TabuadaDeMultiplicação {
-
-}
--- TrianguloDeAsterisco.java
+++ TrianguloDeAsterisco.java
@@ -3,8 +3,13 @@ package Praticando;
 public class TrianguloDeAsterisco {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		for (int linha = 0; linha < 20; linha++) {
+			for (int coluna = 0; coluna <= linha; coluna++) {
+				System.out.print("*");
+			}
+			System.out.println();
+
+		}
 
 	}
-
 }
